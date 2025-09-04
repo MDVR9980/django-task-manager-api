ed2a1b7380c527220367b754089be29090fac9ee
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-09-04 14:28:11 +0330

    update

---
+++ drf_api_project/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    # path('', views.apiOverview, name="api-overview"),
+
+    # for products :
+    path('product-list/', views.productList, name="product-list"),
+    path('product-detail/<int:pk>', views.productDetail, name="product-detail"),
+    # for orders :
+    path('order-list/', views.orderList, name="order-list"),
+]
---
+++ drf_api_project/api/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.serializers import TaskSerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
+from api.models import Task, Product, Order, OrderItem
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        "Delete": '/task-delete/<str:pk>/',
+        }
+
+    return Response(api_urls)
+
+# for products:
+@api_view(['GET'])
+def productList(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def productDetail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+
+
+# for orders:
+@api_view(['GET'])
+def orderList(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
