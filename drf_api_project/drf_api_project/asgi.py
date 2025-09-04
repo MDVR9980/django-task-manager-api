57802c9d8e83a7d7686f24dd0da4bda5a28a8a60
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-09-04 14:17:16 +0330

    update

--- drf_api_project/drf_api_project/asgi.py
+++ drf_api_project/drf_api_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dt_api project.
+ASGI config for drf_api_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dt_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api_project.settings')
 
 application = get_asgi_application()
--- drf_api_project/drf_api_project/wsgi.py
+++ drf_api_project/drf_api_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dt_api project.
+WSGI config for drf_api_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dt_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api_project.settings')
 
 application = get_wsgi_application()
