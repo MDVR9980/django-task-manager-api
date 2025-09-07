b749402821303c5782b64771d73eb6aa4ac9016f
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-09-07 19:53:56 +0330

    update and add swagger-ui

--- drf_api_project/drf_api_project/urls.py
+++ drf_api_project/drf_api_project/urls.py
@@ -4,10 +4,20 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import ( 
+    SpectacularAPIView, 
+    SpectacularRedocView, 
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('api.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
     path('api/token/', TokenObtainPairView.as_view(), name="toekn-obtain-pair"),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name="token-refresh")
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token-refresh"),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(), name='redoc')
 ]
